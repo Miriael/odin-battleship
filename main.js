3bf61a688de89d0dbfec62a415e68ffa335a1b5e
Miriael <ravafiel@protonmail.com>
2023-03-27 13:35:21 +0200

    Implement ship and gameboard factories

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
 import './style.css'
+
+const shipFactory = (length) => {
+  let hitCount = 0
+  const hit = () => hitCount += 1
+  const isSunk = () => {return (hitCount >= length) ? true: false}
+  return { length, isSunk, hit }
+}
+
+const gameboardFactory = () => {
+  let board = [['','','','','','','','','',''],
+               ['','','','','','','','','',''],
+               ['','','','','','','','','',''],
+               ['','','','','','','','','',''],
+               ['','','','','','','','','',''],
+               ['','','','','','','','','',''],
+               ['','','','','','','','','',''],
+               ['','','','','','','','','',''],
+               ['','','','','','','','','',''],
+               ['','','','','','','','','','']]
+  const place = (x, y, ship, orientation) => {
+    for(let i = 0; i < ship.length; i++){
+      if(orientation == 'h'){
+        board[x][y+i] = ship
+      } else if(orientation == 'v'){
+        board[x+i][y] = ship
+      }  
+    }
+  }
+  
+  return { place, board }
+}
+
+export { shipFactory, gameboardFactory }
--- main.test.js
+++ main.test.js
@@ -1,1 +1,40 @@
 import { test, expect, describe } from 'vitest'
+import { shipFactory, gameboardFactory } from './main.js'
+
+describe('Ship object factory tests', () => {
+  test('Length', () => {
+    expect(shipFactory(3).length).toBe(3)
+  })
+  test('Is it sunk when hits match length', () => {
+    let testShip = shipFactory(3)
+    testShip.hit()
+    testShip.hit()
+    testShip.hit()
+    expect(testShip.isSunk()).toBe(true)
+  })
+  test('Is it sunk when not damaged enough', () => {
+    let testShip = shipFactory(3)
+    testShip.hit()
+    testShip.hit()
+    expect(testShip.isSunk()).toBe(false)
+  })
+})
+
+describe('Gameboard factory tests', () => {
+  test('Check if a ship is placed at the coordinates horizontally', () => {
+    let testShip = (shipFactory(3))
+    let board = gameboardFactory()
+    board.place(0, 0, testShip, 'h')
+    expect(board.board[0][0]).toBe(testShip)
+    expect(board.board[0][1]).toBe(testShip)
+    expect(board.board[0][2]).toBe(testShip)
+  })
+  test('Check if a ship is placed at the coordinates vertically', () => {
+    let testShip = (shipFactory(3))
+    let board = gameboardFactory()
+    board.place(0, 0, testShip, 'v')
+    expect(board.board[0][0]).toBe(testShip)
+    expect(board.board[1][0]).toBe(testShip)
+    expect(board.board[2][0]).toBe(testShip)
+  })
+})
