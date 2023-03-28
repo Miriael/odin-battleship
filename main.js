0df89d6c381ba36367424561b4a11df0fe3f06bc
Miriael <ravafiel@protonmail.com>
2023-03-28 15:00:54 +0200

    Add a check for placement on edges

--- main.js
+++ main.js
@@ -18,13 +18,23 @@ const gameboardFactory = () => {
                ['','','','','','','','','',''],
                ['','','','','','','','','',''],
                ['','','','','','','','','','']]
-  const place = (x, y, ship, orientation) => {
-    for(let i = 0; i < ship.length; i++){
-      if(orientation == 'h'){
-        board[x][y+i] = ship
-      } else if(orientation == 'v'){
-        board[x+i][y] = ship
-      }  
+  const place = (y, x, ship, orientation) => {
+    if(orientation == 'v' && (ship.length + y > 10)){
+      for(let i = 10-ship.length; i < 10; i++){
+        board[i][x] = ship
+      }
+    } else if(orientation == 'h' && (ship.length + x > 10)){
+      for(let i = 10-ship.length; i < 10; i++){
+        board[y][i] = ship
+      }
+    } else {
+      for(let i = 0; i < ship.length; i++){
+        if(orientation == 'h'){
+          board[y][x+i] = ship
+        } else if(orientation == 'v'){
+          board[y+i][x] = ship
+        }  
+      }
     }
   }
   
